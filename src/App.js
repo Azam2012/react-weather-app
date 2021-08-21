7d9d6df2c93b8d6e48e0d6126fbc4924fdee9e04
Azam2012 <ghasemighasemi195@gmail.com>
2021-08-21 19:46:27 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default  function App() {
 
   return (
     <div className="App">
+      <div className="container">
   <h1>Weather App</h1>
   <footer> 
     Tis project was coded by Azam and is open-soursed on  {""}
@@ -12,6 +13,7 @@ export default  function App() {
 GitHub
     </a>
   </footer>
+  </div>
     </div>
   );
 }
