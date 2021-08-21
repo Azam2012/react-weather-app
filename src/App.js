ba8fb4b659a1dea07120942754cad88288d16681
Azam2012 <ghasemighasemi195@gmail.com>
2021-08-21 17:33:45 +0200

    added Github repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ export default  function App() {
   return (
     <div className="App">
   <h1>Weather App</h1>
+  <footer> 
+    Tis project was coded by Azam and is open-soursed on  {""}
+    <a href="https://github.com/Azam2012/react-weather-app" target="_blank">
+GitHub
+    </a>
+  </footer>
     </div>
   );
 }
