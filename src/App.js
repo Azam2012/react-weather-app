81371f4681945d30d19912ca118f79a37758ab4e
Azam2012 <ghasemighasemi195@gmail.com>
2021-08-22 23:30:04 +0200

    Added Netlify URL

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ export default  function App() {
   return (
     <div className="App">
       <div className="container">
- 
   <Weather  defaultCity= "Berlin"/>
   <footer > 
     This project was coded by Azam and is open-soursed on  {""}
-    <a href="https://github.com/Azam2012/react-weather-app" target="_blank">
-GitHub
-    </a>
+    <a href="https://github.com/Azam2012/react-weather-app" target="_blank"> GitHub </a> and <a href="https://loving-lamport-fbcf4f.netlify.app/"> hosted on Netlify</a>
+
+   
   </footer>
   </div>
     </div>
