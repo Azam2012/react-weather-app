8576d153860c00a44e0668b4c1682bb5a83a76fc
Azam2012 <ghasemighasemi195@gmail.com>
2021-08-23 13:57:13 +0200

    Changed one element from Weather.js component

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ import "./Weather.css";
 export default function Weather(props) {
     const [weatherData , setWeatherData] = useState({ready: false})
     const [city, setCity] = useState (props.defaultCity)
+    const [city, setCity] = useState(props.defaultCity)
     
    
 function handleResponse(response) {
@@ -15,7 +16,6 @@ function handleResponse(response) {
         date : new Date( response.data.dt * 1000) ,
         temperature : response.data.main.temp,
         description : response.data.weather[0].description,
-        icon : `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         icon :response.data.weather[0].icon,
         humidity : response.data.main.humidity,
         wind : response.data.wind.speed,
@@ -31,7 +31,7 @@ function handleSubmit(event) {
 event.preventDefault()
 search(city);
 }
-function handlecityChange(event){
+function handleCityChange(event){
     setCity(event.target.value);
    
 
@@ -42,7 +42,7 @@ if (weatherData.ready) {
             <form onSubmit={handleSubmit} >
                 <div className="row">
                     <div className="col-9">
-                <input  type="search" placeholder="Enter a city" className="form-control" autoFocus="on" onChange={handlecityChange}  />
+                <input  type="search" placeholder="Enter a city" className="form-control" autoFocus="on" onChange={handleCityChange}  />
                 </div>
                 <div className="col-3">
  <input type="Submit"   value="search" className="btn btn-primary  w-100"/>
