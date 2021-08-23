13e1c06da30be9f5ea98663bac64d6e349bfdfb7
Azam2012 <ghasemighasemi195@gmail.com>
2021-08-23 16:06:30 +0200

    Debugged

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,6 @@ import "./Weather.css";
 export default function Weather(props) {
     const [weatherData , setWeatherData] = useState({ready: false})
     const [city, setCity] = useState (props.defaultCity)
-       const [city, setCity] = useState(props.defaultCity)
 
 
     function handleResponse(response) {
