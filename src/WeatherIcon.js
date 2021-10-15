a5ab4984552505dd396d6160905f9b1e6bcb340c
Azam2012 <ghasemighasemi195@gmail.com>
2021-10-15 16:36:31 +0200

    Deleted invalid icon codes

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -5,8 +5,6 @@ export default function WeatherIcon(props) {
   const codeMapping = {
     "01d": "CLEAR_DAY",
     "01n": "CLEAR_NIGHT",
-    "02d": "FEW_CLOUDS",
-    "02n": "FEW_CLOUDS",
     "03d": "FEW_CLOUDS",
     "03n": "FEW_CLOUDS",
     "04d": "CLOUDY",
